f8d0646e83970ed472def38871bc46e371cd9e87
Gabriel Bacon <gabebacon12@gmail.com>
2018-11-30 23:56:00 -0800

    Initial Push

---
+++ Music/src/Driving/Driver.java
@@ -0,0 +1,16 @@
+package Driving;
+import MyMusic.MyScore;
+import jm.JMC;
+import jm.music.data.Note;
+import jm.util.Play;
+
+public class Driver {
+
+	public static void main(String[] args) {
+
+		MyScore score = new MyScore();
+		// Chedfyyff
+
+	}
+	
+}
